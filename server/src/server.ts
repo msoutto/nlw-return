00c6a935c91dd782095dede7ab1841dad80fa8e9
Matheus Soutto <matheussoutto27@gmail.com>
2022-05-05 20:59:18 -0300

    3- Adding nodemailer and feedbacks req

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,44 @@
 import express from 'express';
+import { prisma } from './prisma';
+import nodemailer from 'nodemailer';
 
 const app = express();
 
-app.get('/users', (req, res) => {
-    return res.send('Hello World');
+app.use(express.json());
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "b26c6090b3e2d9",
+    pass: "9eeb9aa6d4fb41"
+  }
+});
+
+app.post('/feedbacks', async (req, res) => {
+    const { type, comment, screenshot } = req.body;
+
+    const feedback = await prisma.feedback.create({
+        data: {
+            type,
+            comment,
+            screenshot
+        }
+    });
+
+    await transport.sendMail({
+        from: 'Equipe Feedget <oi@feedget.com>',
+        to: 'Matheus Soutto <dev.msoutto@gmail.com>',
+        subject: 'Novo Feedback',
+        html: [
+            `<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
+            `<p>Tipo do feedback: ${type}</p>`,
+            `<p>Comentário: ${comment}</p>`,
+            `</div>`
+        ].join('\n')
+    });
+
+    return res.status(201).json({ data: feedback });
 });
 
 app.listen(3333, () => {
