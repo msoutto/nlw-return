64f13e148c6d4ef473f071a7b42676bb344660fe
Matheus Soutto <matheussoutto27@gmail.com>
2022-05-04 23:08:26 -0300

    2- Creating content from

--- web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,18 +1,22 @@
-import { ArrowLeft } from "phosphor-react";
+import { ArrowLeft, Camera } from "phosphor-react";
 import { FeedbackType, feedbackTypes } from "..";
 import { CloseButton } from "../../CloseButton";
 
 interface FeedbackContentStepProps {
     feedbackType: FeedbackType;
+    onFeedbackRestartRequested: () => void;
 }
 
-export function FeedbackContentStep({ feedbackType }: FeedbackContentStepProps) {
+export function FeedbackContentStep({ feedbackType, onFeedbackRestartRequested }: FeedbackContentStepProps) {
     const feedbackTypeInfo = feedbackTypes[feedbackType];
 
     return (
         <>
             <header>
-                <button type="button" className="top-5 left-5 absolute text-zinc-400 hover:text-zinc-100" >
+                <button type="button" 
+                    className="top-5 left-5 absolute text-zinc-400 hover:text-zinc-100" 
+                    onClick={onFeedbackRestartRequested}
+                >
                     <ArrowLeft weight="bold" className="w-4 h-4" />
                 </button>
 
@@ -23,9 +27,26 @@ export function FeedbackContentStep({ feedbackType }: FeedbackContentStepProps) 
 
                 <CloseButton />
             </header>
-            <div className="flex py-8 gap-2 w-full">
-                
-            </div>
+            <form className="my-4 w-full">
+                <textarea 
+                    className="min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline-none resize-none scrollbar scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin"
+                    placeholder="Conte com detalhes o que está acontecendo..."
+                >
+
+                </textarea>
+
+                <footer className="flex gap-2 mt-2">
+                    <button type="button" className="p-2 bg-zinc-800 rounded-md border-transparent hover:bg-zinc-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors" >
+                        <Camera className="w-6 h-6 text-zinc-900" />
+                    </button>
+                    <button
+                        type="submit"
+                        className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex-justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors"
+                    >
+                        Enviar Feedback
+                    </button>
+                </footer>
+            </form>
         </>
     );
 }
