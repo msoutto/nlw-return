5a08e2645eec9c4438c540467a125413b53270ac
Matheus Soutto <matheussoutto27@gmail.com>
2022-05-07 01:02:11 -0300

    Adding cors limitation

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,10 +4,14 @@ import { routes } from './routes';
 
 const app = express();
 // TODO: limit access
-app.use(cors(/*{
-    origin: '';
-}*/));
+app.use(cors(), (req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "https://nlw-return-dun.vercel.app/");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    return next();
+});
+
 app.use(express.json());
+
 app.use(routes);
 
 app.listen(process.env.PORT || 3333, () => {
