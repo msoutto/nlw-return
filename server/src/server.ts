0226af70ab5fcb14fe34a62f98ce544a643ab435
Matheus Soutto <matheussoutto27@gmail.com>
2022-07-16 00:18:28 -0300

    Gitpod - adding gitpod url regex on cors policy

--- server/src/server.ts
+++ server/src/server.ts
@@ -7,8 +7,9 @@ const app = express();
 // TODO: limit access
 app.use(cors(), (req, res, next) => {
     const allowedOrigins = ['https://nlw-return-dun.vercel.app', 'http://localhost:3000', 'https://3000-matheussoutto-nlwreturn*.gitpod.io'];
+    const gitpodOrigins = /^https:\/\/3000-matheussoutto-nlwreturn-.+\.gitpod\.io$/
     const origin = req.headers.origin ?? '';
-    if (allowedOrigins.includes(origin)) {
+    if (allowedOrigins.includes(origin) || gitpodOrigins.test(origin)) {
         res.setHeader('Access-Control-Allow-Origin', origin);
     }
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
