1ac665e1a78ba32b7054be08c36cf55e63a0adfb
Matheus Soutto <matheussoutto27@gmail.com>
2022-07-16 00:24:56 -0300

    Gitpod - logging cors origin validation

--- server/src/server.ts
+++ server/src/server.ts
@@ -9,8 +9,11 @@ app.use(cors(), (req, res, next) => {
     const allowedOrigins = ['https://nlw-return-dun.vercel.app', 'http://localhost:3000', 'https://3000-matheussoutto-nlwreturn*.gitpod.io'];
     const gitpodOrigins = /^https:\/\/3000-matheussoutto-nlwreturn-.+\.gitpod\.io$/
     const origin = req.headers.origin ?? '';
+    console.log(origin);
+
     if (allowedOrigins.includes(origin) || gitpodOrigins.test(origin)) {
         res.setHeader('Access-Control-Allow-Origin', origin);
+        console.log('passed');
     }
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     return next();
