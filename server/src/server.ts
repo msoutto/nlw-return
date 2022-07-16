1975d17b598480a1d3c3ad4697f52c5d55c62963
Matheus Soutto <matheussoutto27@gmail.com>
2022-07-16 14:16:27 -0300

    Removing unnecessary stuff from back end server

--- server/src/server.ts
+++ server/src/server.ts
@@ -10,20 +10,12 @@ dotenv.config();
 // TODO: limit access
 app.use(cors(), (req, res, next) => {
     const allowedOrigins = ['https://nlw-return-dun.vercel.app', 'https://3000-matheussoutto-nlwreturn*.gitpod.io'];
-    const gitpodOrigins = /^https:\/\/3000-matheussoutto-nlwreturn-.+\.gitpod\.io[\/]?$/
-    let urlTest = 'https://3000-matheussoutto-nlwreturn-dcu2v9iv5zg.ws-us54.gitpod.io';
     const origin = req.headers.origin ?? '';
-    const referrer = req.headers.referer ?? '';
     const env_url = process.env.WEB_URL ?? '';
-    console.log(env_url);
-    console.log(req.headers);
 
-    if (gitpodOrigins.test(urlTest))
-        console.log('test passed');
-
-    if (allowedOrigins.includes(origin) || origin.includes(env_url) || referrer.includes(env_url)) {
+    if (allowedOrigins.includes(origin) || origin.includes(env_url)) {
         res.setHeader('Access-Control-Allow-Origin', origin);
-        console.log('passed');
+        console.log('passed: ', origin);
     }
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     return next();
